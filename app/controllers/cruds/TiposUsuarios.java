9b2a19b90f3981580127aa90e6c9de2eab2a7c32
joseacuna <joseacunaacuna@gmail.com>
2015-11-14 16:47:11 -0300

    actualizacion

---
+++ app/controllers/cruds/TiposUsuarios.java
@@ -0,0 +1,11 @@
+package controllers.cruds;
+
+import controllers.CRUD;
+import models.TipoUsuario;
+
+/**
+ * Created by Jose on 14-11-15.
+ */
+@CRUD.For(TipoUsuario.class)
+public class TiposUsuarios extends CRUD {
+}
